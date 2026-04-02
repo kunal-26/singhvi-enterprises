053fc3459e8accb44e5264221b7074c249d13019
Kunal Jain <kunajain2608@gmail.com>
2026-04-02 20:03:46 +0530

    added commit

--- supabase.js
+++ supabase.js
@@ -3,7 +3,10 @@
 const SUPABASE_URL = "https://nesjyignqnttnjthnmps.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lc2p5aWducW50dG5qdGhubXBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMjI2NTIsImV4cCI6MjA5MDY5ODY1Mn0.o7HRWVO96nnrnc_9zgLgGG55en2IptKPWYtGn_803pY";
 
-const supabaseClient = supabase.createClient(
+/* ---------- LOAD SUPABASE CLIENT ---------- */
+
+/* create global client */
+window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_KEY
+    SUPABASE_ANON_KEY
 );
